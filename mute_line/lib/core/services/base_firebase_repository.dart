38f868eebbf5deb2bbfdd11b2d859ab7e35e1d3e
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2025-12-05 20:18:29 +0530

    remove firebase auth

--- mute_line/lib/core/services/base_firebase_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-abstract class BaseFirebaseRepository {
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-}
--- mute_line/lib/features/auth/repository/mute_line_auth_remote_repository.dart
+++ mute_line/lib/features/auth/repository/mute_line_auth_remote_repository.dart
@@ -1,6 +1,1 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mute_line/core/services/base_firebase_repository.dart';
-
-class MuteLineAuthRemoteRepository extends BaseFirebaseRepository {
-  Stream<User?> get authStateChanges => auth.authStateChanges();
-}
+class MuteLineAuthRemoteRepository {}
